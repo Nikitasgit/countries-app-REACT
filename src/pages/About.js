5eb692829037859ec5e8b33c8cd4e56f717b3a31
Victor Leman <victorleman1@gmail.com>
2023-05-23 10:24:38 +0300

    countries-app-react

---
+++ src/pages/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolore
+        repudiandae, tenetur eos quia quos blanditiis? Ipsum provident natus
+        doloribus quas, veritatis placeat vel impedit est aperiam quia atque
+        fugit, hic, non eveniet harum architecto laudantium. Quod, voluptatum
+        corporis tempora laudantium impedit dolor illo soluta nisi excepturi
+        eligendi voluptatem nihil molestias quis vel facilis quaerat asperiores
+        officia alias reprehenderit tempore. Quam eaque ex eum harum. Sunt
+        expedita minima numquam magnam labore perspiciatis. Laudantium sit ad
+        quidem magnam ab? Voluptas velit ratione quos incidunt debitis
+        accusantium laboriosam reiciendis eaque pariatur qui culpa ad voluptates
+        amet nihil eum doloremque, at cum deserunt.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias quos
+        nobis delectus nam. Beatae veritatis aliquid quisquam quas nobis laborum
+        tempora iste temporibus quod! Cupiditate numquam tempore unde! Nobis
+        impedit, sunt vero repellendus, omnis, iusto reiciendis quos neque
+        provident illum nostrum aliquid. Et, dignissimos soluta rerum excepturi
+        non eius, iusto magni aliquam inventore, recusandae quasi tempore sequi
+        neque possimus officia eos. Debitis, libero est. Veniam ipsam, repellat
+        ullam hic labore quidem distinctio a quasi voluptate placeat aliquid
+        dolore rerum nostrum architecto ipsa. Soluta dicta recusandae a modi.
+        Voluptatum autem et, amet minima doloribus dolorem repellendus
+        temporibus nobis odio quis totam.
+      </p>
+    </div>
+  );
+};
+
+export default About;
